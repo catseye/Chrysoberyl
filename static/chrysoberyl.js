95d602c75ef99468e2efe5c2b4958f55923bdf12
catseye <cpressey@gmail.com>
2013-04-13 13:13:13 +0100

    Reduce dependency on jQuery.

--- static/chrysoberyl.js
+++ static/chrysoberyl.js
@@ -1,41 +1,48 @@
 /*
- * Requires jQuery.
- * Can be loaded at the bottom of the page.
+ * Should be loaded at the bottom of the page.
  */
 
 /*
- * Given a CSS class and local URL prefix, apply the class to all links on
- * the page which are considered external.
+ * Given a CSS class and local URL prefix, apply the class to all links
+ * ('a' tags) on the page which are considered external.
  * A link is considered external if it begins with "http://" or "https://"
  * and does not begin with the local URL prefix.
  */
-function add_class_to_external_links(css_class, local_url_prefix) {
-  $('a').each(function (i) {
-    var href = $(this).attr('href');
+function addClassToExternalLinks(cssClass, localUrlPrefix) {
+  var anchors = document.getElementsByTagName('a');
+  for (var i = 0; i < anchors.length; i++) {
+    var elem = anchors[i];
+    var href = elem.href;
+    if (href.substring(0, 16) == "http://127.0.0.1") continue;
     if (href.substring(0, 7) == "http://" || href.substring(0, 8) == "https://") {
-      if (href.substring(0, local_url_prefix.length) != local_url_prefix) {
-        $(this).addClass(css_class);
+      var prefix = href.substring(0, localUrlPrefix.length);
+      if (prefix != localUrlPrefix) {
+        elem.className += elem.className ? (' ' + cssClass) : cssClass;
       }
     }
-  });
+  }
 }
 
 /*
- * Given a block element specifier, expand it so that the height of
- * the body matches the height of the displayed page.
+ * Given two block elements, expand the first so that the second takes up
+ * the remaining height of the visible document.
  * This is aesthetically nice for pushing footers down to the bottom of the
  * page, on short pages.
  * This is designed to only be run once, at window load time, because as
  * they say, browser resizing is a developer activity.
  */
-function expand_element(element) {
-  var extra_room = $(window).height() - $('body').height();
+function expandElementToFillDocument(article, footer) {
+  extra_room =
+      (document.documentElement.clientHeight - footer.clientHeight) -
+      footer.offsetTop;
+  newHeight = (article.clientHeight + extra_room) + "px";
   if (extra_room > 0) {
-    $(element).height($(element).height() + extra_room);
+    article.style.height = newHeight;
   }
 }
 
-$(window).load(function() {
-  add_class_to_external_links('external', "http://catseye.tc");
-  expand_element('article');
-});
+addClassToExternalLinks('external', "http://catseye.tc");
+expandElementToFillDocument(
+  document.getElementsByTagName('article')[0],
+  document.getElementsByTagName('footer')[0]
+);
