3e2a9df117e018ae980553e1420cb29e24bdea04
Chris Pressey <cpressey@catseye.tc>
2017-10-04 16:17:22 +0100

    This is basically a note to self.

---
+++ script/cleanup.sh
@@ -0,0 +1,7 @@
+# note to self:
+
+feedmark --input-refdex=retrocomputing-refdex.json article/Games.md --rewrite-markdown
+
+# we actually need multiple input refdexes / way to combine them.
+# and we probably need a schema.
+# see The Dossier.
